0c675780dea4777078f3b72fe3ea4aef0afdbeef
Haris Mexis <theoharismexis@hotmail.com>
2021-06-19 22:25:24 +0100

    updates

--- app/src/androidTest/java/com/harismexis/magic/setup/base/InstrumentedTestSetup.kt
+++ app/src/androidTest/java/com/harismexis/magic/setup/base/InstrumentedTestSetup.kt
@@ -1,9 +1,7 @@
 package com.harismexis.magic.setup.base
 
-import com.harismexis.magic.parser.MockCardsParser
 import com.harismexis.magic.setup.testutil.InstrumentedFileParser
 
 open class InstrumentedTestSetup {
     protected val fileParser = InstrumentedFileParser()
-    protected val herosParser = MockCardsParser(fileParser)
 }
